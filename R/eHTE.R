3d5175eaa93a23890eda9bfc030359ff600217d7
Chris Chatham <chrischatham@Chris-Chatham-MacBook-Pro.local>
2024-06-02 18:05:36 -0700

    Added testHTE method

--- R/eHTE.R
+++ R/eHTE.R
@@ -5,7 +5,7 @@
 #' @param placebo_arm A vector of patient-level treatment effects from the control condition (e.g., placebo).
 #' @param drug_arm A vector of patient-level treatment effects from the active condition (e.g., active drug).
 #' @param pctiles A vector of percentiles at which to evaluate the drug/placebo difference. Default is a vector of percentiles between the 3rd and 97th at increments of 2 (as in Siegel et al 2024).
-#' @return An custom object of class 'eHTE_class', which accepts methods summary(), plot(), and print().
+#' @return A custom object of class 'eHTE_class', which accepts methods summary(), plot(), and print().
 #' @export
 estimateHTE <- function(placebo_arm,drug_arm,pctiles=seq(from=3,to=97,by=2)/100){
   n_pbo <- length(placebo_arm)
@@ -19,7 +19,39 @@ estimateHTE <- function(placebo_arm,drug_arm,pctiles=seq(from=3,to=97,by=2)/100)
                  n_act = n_act,
                  placebo_arm=placebo_arm,
                  drug_arm=drug_arm,
-                 pctiles=pctiles)
+                 pctiles=pctiles,
+                 pval=NULL)
+  class(result) <- "eHTE_class"
+  return(result)
+}
+
+#' testHTE
+#'
+#' Compute a p-value of an obtained eHTE estimate, using non-parametric permutation. 
+#'
+#' @param estimateHTE_result An object of class eHTE_class - for example, the result of estimateHTE()
+#' @param n_perm The number of permutations desired. Default = 100.
+#' @return A custom object of class 'eHTE_class', which accepts methods summary(), plot(), and print().
+#' @export
+testHTE <- function(estimateHTE_result,n_perm=100){
+  perm_arm_ur <- c(estimateHTE_result$placebo_arm,estimateHTE_result$drug_arm)
+  pb = txtProgressBar(min = 0, max = n_perm, initial = 0,style = 3) 
+  for(n in 1:n_perm){
+    perm_arm <- sample(perm_arm_ur)
+    placebo_arm <- perm_arm[1:n_pbo]
+    drug_arm <- perm_arm[(n_pbo+1):length(perm_arm)]
+    placebo_sd <- sd(placebo_arm)
+    null_eHTE[n] <- eHTE_estimate(placebo_arm,drug_arm,pctiles,placebo_sd)
+    setTxtProgressBar(pb,n)
+  }
+  close(pb)
+  result <- list(eHTE = estimateHTE_result$eHTE,
+                 n_pbo = estimateHTE_result$n_pbo,
+                 n_act = estimateHTE_result$n_act,
+                 placebo_arm=estimateHTE_result$placebo_arm,
+                 drug_arm=estimateHTE_result$drug_arm,
+                 pctiles=estimateHTE_result$pctiles,
+                 pval=length(which(null_eHTE>=estimateHTE_result$eHTE))/length(null_eHTE))
   class(result) <- "eHTE_class"
   return(result)
 }
