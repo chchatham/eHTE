78991bbf6ac18e661a99ccffbba5a2636984fdf1
Chris Chatham <chrischatham@Chris-Chatham-MacBook-Pro.local>
2024-06-02 17:51:20 -0700

    Updating package level help

---
+++ R/package-doc.R
@@ -0,0 +1,17 @@
+#' eHTE: A Package for Estimating Heterogeneous Treatment Effects
+#'
+#' The eHTE package provides tools for estimating heterogeneous treatment effects.
+#' This work is based on Siegel et al, 2024: https://www.medrxiv.org/content/10.1101/2024.04.23.24306211v1
+#'
+#' @section Functions:
+#' The main functions in this package are:
+#' \itemize{
+#'   \item \code{\link{estimateHTE}}: Estimates the eHTE metric of Siegel et al.
+#'   \item \code{\link{testHTE}}: Tests the eHTE metric using the non-parameteric permutation method described by Siegel et al.  
+#' }
+#'
+#' @docType package
+#' @name eHTE
+#' @aliases eHTE-package
+#' @keywords package
+"_PACKAGE"
