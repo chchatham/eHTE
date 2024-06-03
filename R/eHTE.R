ca95adebd9019415824b68a7077768b968c9d5c1
Chris Chatham <chrischatham@Chris-Chatham-MacBook-Pro.local>
2024-06-02 18:22:39 -0700

    Added plot eHTE method

--- R/eHTE.R
+++ R/eHTE.R
@@ -5,7 +5,7 @@
 #' @param placebo_arm A vector of patient-level treatment effects from the control condition (e.g., placebo).
 #' @param drug_arm A vector of patient-level treatment effects from the active condition (e.g., active drug).
 #' @param pctiles A vector of percentiles at which to evaluate the drug/placebo difference. Default is a vector of percentiles between the 3rd and 97th at increments of 2 (as in Siegel et al 2024).
-#' @return A custom object of class 'eHTE_class', which accepts methods summary(), plot(), and print().
+#' @return A custom object of class 'eHTE_class', which accepts methods summary(), plot(), and print(). P-value will be null until testHTE() is run on the result.
 #' @export
 estimateHTE <- function(placebo_arm,drug_arm,pctiles=seq(from=3,to=97,by=2)/100){
   n_pbo <- length(placebo_arm)
@@ -27,7 +27,7 @@ estimateHTE <- function(placebo_arm,drug_arm,pctiles=seq(from=3,to=97,by=2)/100)
 
 #' testHTE
 #'
-#' Compute a p-value of an obtained eHTE estimate, using non-parametric permutation. 
+#' Compute a p-value of an obtained eHTE estimate, using non-parametric permutation of the individuals in the placebo/control and drug/active arms. This p-value represents the proportion of observations from the permuted null distribution which are equally or more extreme than the value estimated using estimateHTE(). As such, this is a one-sided p-value for testing whether there is greater evidence for treatment effect heterogeneity in the active than in the control condition.
 #'
 #' @param estimateHTE_result An object of class eHTE_class - for example, the result of estimateHTE()
 #' @param n_perm The number of permutations desired. Default = 100.
@@ -59,13 +59,22 @@ testHTE <- function(estimateHTE_result,n_perm=100){
 # Custom print method for class eHTE_class
 #' @export
 print.eHTE_class <- function(x){
-  print(x[1:3])
+  print(x)
 }
 
 # Custom plot method for class eHTE_class
 #' @export
 plot.eHTE_class <- function(x){
-  plot(x[4])
+  ecdf_drug <- ecdf(x$drug_arm)
+  ecdf_pbo <- ecdf(x$placebo_arm)
+  layout_matrix <- matrix(c(1, 2), nrow = 1, ncol = 2)
+  layout_widths <- c(2, 1)  # Left column twice as wide as the right column
+  plot(ecdf_drug, main = "Empirical Cumulative Distribution Functions", xlab="Sx Score", ylab="Cumulative Percentile", col="red")
+  lines(ecdf_pbo, col="blue")
+  legend("bottomright", legend = c("Placebo (control)", "Drug (active)"),
+         col = c("blue", "red"), lwd = 2)
+  hist(x$drug_arm)
+  layout(1)
 }
 
 # Custom summary method for class eHTE_class
@@ -73,10 +82,10 @@ plot.eHTE_class <- function(x){
 summary.eHTE_class <- function(x){
   cat("Summary of eHTE estimate (package eHTE)\n\n")
   cat("Input Parameters:\n")
-  cat(paste0("Number of Percentiles: ",length(x[6]),"\n",sep=""))
-  cat(paste0("Range of Percentiles Used: ",min(x[6])," to ",max(x[6]),"\n",sep=""))
-  cat(paste0("Number of Observations, Control Condition (e.g., placebo): ",x[2],"\n",sep=""))
-  cat(paste0("Number of Observations, Active Condition (e.g., drug): ",x[3],"\n",sep=""))
+  cat(paste0("Number of Percentiles: ",length(x$pctiles),"\n",sep=""))
+  cat(paste0("Range of Percentiles Used: ",min(x$pctiles)," to ",max(x$pctiles),"\n",sep=""))
+  cat(paste0("Number of Observations, Control Condition (e.g., placebo): ",x$n_pbo,"\n",sep=""))
+  cat(paste0("Number of Observations, Active Condition (e.g., drug): ",x$n_act,"\n",sep=""))
   cat("Output:\n")
-  cat(paste0("eHTE estimate: ",x[1],sep=""))
+  cat(paste0("eHTE estimate: ",x$eHTE,sep=""))
 }
